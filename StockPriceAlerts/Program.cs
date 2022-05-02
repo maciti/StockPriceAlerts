c88b90692b928e141530b2de777b6924c8ef4a54
maciti <misterwf@protonmail.com>
2022-05-02 10:32:06 +0000

    stock price alerts - console app

---
+++ StockPriceAlerts/Program.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using StockPriceAlerts;
+using System.Reflection;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+var logs = DateTime.Now.ToString();
+
+var currentLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+var stockAlertsSettings = JsonConvert.DeserializeObject<StockAlertSettings>(File.ReadAllText($"{currentLocation}/stockAlertsSettings.json"));
+
+try
+{
+    var alphaVantageQuery = $"query?function=GLOBAL_QUOTE&symbol=##symbol##&apikey={stockAlertsSettings.AlphaVantage.ApiKey}";
+
+    var alert = string.Empty;
+
+    var alphaVantageClient = new HttpClient { BaseAddress = new Uri("https://www.alphavantage.co/") };
+
+    foreach (var priceAlert in stockAlertsSettings.PriceAlerts)
+    {
+        var result = await alphaVantageClient.GetAsync(alphaVantageQuery.Replace("##symbol##", priceAlert.StockSymbol));
+
+        dynamic globalQuote = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(await result.Content.ReadAsStringAsync())["Global Quote"];
+
+        var quoteDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(globalQuote.ToString());
+
+        var currentPrice = Convert.ToDecimal(quoteDict["05. price"]);
+
+        if (priceAlert.PriceLowerThan.HasValue && currentPrice < priceAlert.PriceLowerThan)
+        {
+            alert += $"{priceAlert.StockSymbol}:{currentPrice.ToString("F2")} ";
+        }
+
+        if (priceAlert.PriceHigherThan.HasValue && currentPrice > priceAlert.PriceHigherThan)
+        {
+            alert += $"{priceAlert.StockSymbol}:{currentPrice.ToString("F2")} ";
+        }
+    }
+
+    if (!string.IsNullOrEmpty(alert))
+    {
+        TwilioClient.Init(stockAlertsSettings.Twilio.AccountSid, stockAlertsSettings.Twilio.AuthToken);
+
+        var message = MessageResource.Create(
+            body: alert,
+            from: new Twilio.Types.PhoneNumber(stockAlertsSettings.Twilio.SmsFrom),
+            to: new Twilio.Types.PhoneNumber(stockAlertsSettings.Twilio.SmsTo)
+        );
+
+        logs += $" - MESSAGE SID: {message.Sid}, BODY: {alert}, SENT ";
+    }
+
+    logs += " - JOB EXECUTED!";
+
+}
+catch (Exception ex)
+{
+    logs += ex.Message;
+}
+finally
+{
+    if (stockAlertsSettings.LogOnFile)
+    {
+        using (File.OpenWrite($"{currentLocation}/logs.txt")) ;
+
+        File.AppendAllText($"{currentLocation}/logs.txt", logs + Environment.NewLine);
+    }
+}
---
+++ StockPriceAlerts/StockAlertsSettings.cs
@@ -0,0 +1,37 @@
+﻿namespace StockPriceAlerts;
+
+public class StockAlertSettings
+{
+    public AlphaVantage AlphaVantage { get; set; }
+
+    public Twilio Twilio { get; set; }
+
+    public List<PriceAlert> PriceAlerts { get; set; }
+
+    public bool LogOnFile { get; set; }
+}
+
+public class AlphaVantage
+{
+    public string ApiKey { get; set; }
+}
+
+public class Twilio
+{
+    public string AccountSid { get; set; }
+
+    public string AuthToken { get; set; }
+
+    public string SmsFrom { get; set; }
+
+    public string SmsTo { get; set; }
+}
+
+public class PriceAlert
+{
+    public string StockSymbol { get; set; }
+
+    public decimal? PriceLowerThan { get; set; }
+
+    public decimal? PriceHigherThan { get; set; }
+}
